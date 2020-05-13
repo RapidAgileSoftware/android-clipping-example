4fd4044cf86f44fc8ce637f8a7a9b7e40f58d604
Jens Krause <jens@rasta.online>
2020-05-13 13:38:45 +0700

    add dimensions for small and bigger screens, add translatable strings to be shown on the clipped area

--- app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
+
+//@JvmOverloads annotation instructs the Kotlin compiler to generate overloads for this function
+// that substitute default parameter values
 class ClippedView @JvmOverloads constructor(
     context: Context,
     attrs:AttributeSet?=null,
